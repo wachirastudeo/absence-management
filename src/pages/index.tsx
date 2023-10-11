737a309d8bc4e74febf3f3308acf7c7f8305e584
wachirastudeo <wachirastudeo@gmail.com>
2023-10-11 21:49:54 +0700

    create todo list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,37 @@
+import { useState } from "react";
 const IndexPage = () => {
-  return <div>Hello world</div>;
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([
+    { id: 1, content: "Todot#1" },
+    { id: 2, content: "Todot#2" },
+    { id: 3, content: "Todot#3" },
+  ]);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={todo}
+        onChange={(event) => setTodo(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          setTodos([...todos, { id: todos.length + 1, content: todo }]);
+
+          setTodo("");
+        }}
+      >
+        add Todo
+      </button>
+      <br />
+      <br />
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default IndexPage;
